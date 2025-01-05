643c5d0a4a3c627f3e0ae6360eb81302055c5e00
soyeonii <dingdingcrong@naver.com>
2025-01-05 21:45:14 +0900

    Class-based Views

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductListAPIView.as_view()),
-    path('products/info/', views.product_info),
+    path('products/info/', views.ProductInfoAPIView.as_view()),
     path('products/<int:product_id>/', views.ProductDetailAPIView.as_view()),
     path('orders/', views.OrderListAPIView.as_view()),
     path('user-orders/', views.UserOrderListAPIView.as_view(), name='user-orders'),
--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 class ProductListAPIView(generics.ListAPIView):
     queryset = Product.objects.exclude(stock__gt=0)
@@ -28,12 +29,12 @@ class UserOrderListAPIView(generics.ListAPIView):
         qs = super().get_queryset()
         return qs.filter(user=self.request.user)
 
-@api_view(['GET'])
-def product_info(request):
-    products = Product.objects.all()
-    serializer = ProductInfoSerializer({
-        'products': products,
-        'count': products.count(),
-        'max_price': products.aggregate(max_price=Max('price'))['max_price']
-    })
-    return Response(serializer.data)
+class ProductInfoAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductInfoSerializer({
+            'products': products,
+            'count': products.count(),
+            'max_price': products.aggregate(max_price=Max('price'))['max_price']
+        })
+        return Response(serializer.data)
